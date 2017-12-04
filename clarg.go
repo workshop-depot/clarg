e2ddece82ffc61718d6d3cdd972ad4922f8e10cc
dc0d <kaveh.shahbazian@gmail.com>
2017-12-04 06:35:29 +0330

    initial code

---
+++ clarg.go
@@ -0,0 +1,64 @@
+// Package clarg provides simple Sub-Commands for Go using flag package
+// - can be used via copy/paste too, because it's small.
+package clarg
+
+import (
+	"flag"
+	"os"
+)
+
+// Parse parses arguments for list of commands.
+// The first command is the default command (top level args) and can be nil.
+func Parse(commands ...*Cmd) error {
+	return parse(os.Args[1:], commands...)
+}
+
+func parse(args []string, commands ...*Cmd) error {
+	if len(commands) > 0 && commands[0] != nil {
+		if err := commands[0].Parse(args); err != nil {
+			return err
+		}
+		args = commands[0].Args()
+	}
+	if len(args) == 0 {
+		return nil
+	}
+	commands = commands[1:]
+	cmdTable := make(map[string]*Cmd)
+	for _, cmd := range commands {
+		cmdTable[cmd.Name] = cmd
+	}
+	for len(cmdTable) > 0 {
+		if len(args) == 0 {
+			return nil
+		}
+		for name, cmd := range cmdTable {
+			if args[0] != name {
+				continue
+			}
+			if err := cmd.Parse(args[1:]); err != nil {
+				return err
+			}
+			args = cmd.Args()
+			delete(cmdTable, name)
+			break
+		}
+	}
+
+	return nil
+}
+
+// New creates a *Cmd, default value for errorHandling is flag.ExitOnError
+func New(name string, errorHandling ...flag.ErrorHandling) *Cmd {
+	eh := flag.ExitOnError
+	if len(errorHandling) > 0 {
+		eh = errorHandling[0]
+	}
+	return &Cmd{FlagSet: flag.NewFlagSet(name, eh), Name: name}
+}
+
+// Cmd represents a sub command
+type Cmd struct {
+	*flag.FlagSet
+	Name string // if *flag.FlagSet could give us the name, there would be no need for this field.
+}
---
+++ clarg_test.go
@@ -0,0 +1,137 @@
+package clarg
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// this is just one way to do it. the flag back variables can be anywhere.
+
+var cmdDefault struct {
+	*Cmd
+	count int
+	data  string
+}
+
+var cmdList struct {
+	*Cmd
+	age  int
+	name string
+}
+
+var cmdSend struct {
+	*Cmd
+	dst     string
+	payload string
+}
+
+func prepCmd() {
+	cmdDefault.Cmd = New("")
+	cmdList.Cmd = New("list")
+	cmdSend.Cmd = New("send")
+
+	cmdDefault.IntVar(&cmdDefault.count, "cnt", 0, "")
+	cmdDefault.StringVar(&cmdDefault.data, "data", "", "")
+
+	cmdList.IntVar(&cmdList.age, "age", -1, "")
+	cmdList.StringVar(&cmdList.name, "name", "", "")
+
+	cmdSend.StringVar(&cmdSend.dst, "dst", "", "")
+	cmdSend.StringVar(&cmdSend.payload, "p", "", "")
+}
+
+func TestNoArg(t *testing.T) {
+	assert := assert.New(t)
+
+	args := []string{}
+	prepCmd()
+
+	err := parse(args, cmdDefault.Cmd, cmdSend.Cmd, cmdList.Cmd)
+	assert.NoError(err)
+}
+
+func TestTopArg(t *testing.T) {
+	assert := assert.New(t)
+
+	args := []string{"-data", "Hi!", "-cnt", "66"}
+	prepCmd()
+
+	err := parse(args, cmdDefault.Cmd, cmdSend.Cmd, cmdList.Cmd)
+	assert.NoError(err)
+	assert.Equal("Hi!", cmdDefault.data)
+	assert.Equal(66, cmdDefault.count)
+}
+
+func TestMultipleCommands01(t *testing.T) {
+	assert := assert.New(t)
+
+	args := []string{"-data", "Hi!", "-cnt", "66",
+		"send", "-dst", "10", "-p", "QWERTY", "list", "-age", "20", "-name", "Kaveh"}
+	prepCmd()
+
+	err := parse(args, cmdDefault.Cmd, cmdSend.Cmd, cmdList.Cmd)
+	assert.NoError(err)
+	assert.Equal("Hi!", cmdDefault.data)
+	assert.Equal(66, cmdDefault.count)
+	assert.Equal("10", cmdSend.dst)
+	assert.Equal("QWERTY", cmdSend.payload)
+	assert.Equal(20, cmdList.age)
+	assert.Equal("Kaveh", cmdList.name)
+}
+
+func TestMultipleCommands02(t *testing.T) {
+	assert := assert.New(t)
+
+	args := []string{"-data", "Hi!", "-cnt", "66",
+		"list", "-age", "20", "-name", "Kaveh",
+		"send", "-dst", "10", "-p", "QWERTY"}
+	prepCmd()
+
+	err := parse(args, cmdDefault.Cmd, cmdSend.Cmd, cmdList.Cmd)
+	assert.NoError(err)
+	assert.Equal("Hi!", cmdDefault.data)
+	assert.Equal(66, cmdDefault.count)
+	assert.Equal("10", cmdSend.dst)
+	assert.Equal("QWERTY", cmdSend.payload)
+	assert.Equal(20, cmdList.age)
+	assert.Equal("Kaveh", cmdList.name)
+}
+
+func ExampleCmd() {
+	cmdDefaultFlags := New("")
+	cmdListFlags := New("list")
+	cmdSendFlags := New("send")
+
+	var cmdDefault struct {
+		count int
+		data  string
+	}
+
+	var cmdList struct {
+		age  int
+		name string
+	}
+
+	var cmdSend struct {
+		dst     string
+		payload string
+	}
+
+	cmdDefaultFlags.IntVar(&cmdDefault.count, "cnt", 0, "")
+	cmdDefaultFlags.StringVar(&cmdDefault.data, "data", "", "")
+
+	cmdListFlags.IntVar(&cmdList.age, "age", -1, "")
+	cmdListFlags.StringVar(&cmdList.name, "name", "", "")
+
+	cmdSendFlags.StringVar(&cmdSend.dst, "dst", "", "")
+	cmdSendFlags.StringVar(&cmdSend.payload, "p", "", "")
+
+	if err := Parse(cmdDefaultFlags,
+		cmdListFlags,
+		cmdSendFlags); err != nil {
+		// show/handle error
+	}
+
+	// use values of back fields for flags
+}
